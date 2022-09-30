036dd869e4e1292e74a3eb73920a7ecb5ee5f1f4
KimRoberts <1916506@swansea.ac.uk>
2022-09-30 13:33:59 +0100

    Updated styles and added delete functionality

--- client/albums-ui/src/components/Album.js
+++ client/albums-ui/src/components/Album.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
-const Album = ({album}) => {
+const Album = ({album, deleteAlbum}) => {
   return (
-    <div>
-        <p>Title: {album.title}</p>
-        <p>Artist: {album.artist}</p>
-        <p>Year: {album.year}</p>
+    <div className="album">
+      <img src="https://upload.wikimedia.org/wikipedia/en/3/3b/Dark_Side_of_the_Moon.png" />
+      <div className="info">
+        <h1>{album.title}</h1>
+        <p>{album.artist}</p>
+        <p>{album.year}</p>
+      </div>
+      <button className='close-btn' onClick={() => deleteAlbum(album.id)}>
+        <span className="material-symbols-outlined">delete_forever</span>
+      </button>
     </div>
-  )
+  );
 }
 
 export default Album
